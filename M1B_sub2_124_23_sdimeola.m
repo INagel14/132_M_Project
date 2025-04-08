d8b12b59553aafd3b35664a66390186467b1a9bc
INagel14 <isaacnagel2024@gmail.com>
2025-04-08 12:20:13 -0400

    Renamed to M1B

---
+++ M1B_sub2_124_23_sdimeola.m
@@ -0,0 +1,50 @@
+function[Output] = M1A_sub2_124_23_sdimeola(Input)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ENGR 132 
+% Run subfunction 3 to find the final and inital speed
+%
+% Function Call
+% [Output] = M1A_sub2_124_23_sdimeola(Input)
+%
+% Input Arguments
+% Input
+%
+% Output Arguments
+% Output
+%
+% Assignment Information
+%   Assignment:     M1A, Problem 4
+%   Team member:    Skylar DiMeola, sdimeola@purdue.edu
+%   Team ID:        124-23
+%   Academic Integrity:
+%     [N/A] We worked with one or more peers but our collaboration
+%        maintained academic integrity.
+%     
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% ____________________
+%% INITIALIZATION
+
+
+%% ____________________
+%% CALCULATIONS
+
+Output = Input .* 3;
+
+%% ____________________
+%% FORMATTED TEXT/FIGURE DISPLAYS
+
+
+%% ____________________
+%% RESULTS
+fprintf('Data successfully passed to subfunction 2 programmed by Skyler DiMeola.\n')
+
+%% ____________________
+%% ACADEMIC INTEGRITY STATEMENT
+% We have not used source code obtained from any other unauthorized
+% source, either modified or unmodified. Neither have we provided
+% access to my code to another. The program we are submitting
+% is our own original work.
+
+
+
---
+++ M1B_sub3_124_23_muell147.m
@@ -0,0 +1,49 @@
+function [Output]=  M1A_sub3_124_23_muell147(Input)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ENGR 132
+%
+% Subfunction 4- finding initial and final velocities, testing function call
+% Function Call
+% [Output]=  M1A_sub3_124_23_muell147(Input)
+%
+% Input Arguments
+% Input
+%
+% Output Arguments
+% Output
+%
+% Assignment Information
+%   Assignment:     M1A, Problem 1
+%   Team member:    Elizabeth Mueller, muell147@purdue.edu [repeat for each person]
+%   Team ID:        124-23
+%   Academic Integrity:
+%     [N/A] We worked with one or more peers but our collaboration
+%        maintained academic integrity.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% ____________________
+%% INITIALIZATION
+
+
+%% ____________________
+%% CALCULATIONS
+Output = Input .* 3;
+
+
+%% ____________________
+%% FORMATTED TEXT/FIGURE DISPLAYS
+
+
+%% ____________________
+%% RESULTS
+fprintf('Data successfully passed to subfunction 3 programmed by Elizabeth Mueller.\n')
+
+%% ____________________
+%% ACADEMIC INTEGRITY STATEMENT
+% We have not used source code obtained from any other unauthorized
+% source, either modified or unmodified. Neither have we provided
+% access to my code to another. The program we are submitting
+% is our own original work.
+
+
+
