aeab4a68b95074bb5311f188fd5a8ff0e452702d
INagel14 <isaacnagel2024@gmail.com>
2025-04-20 14:32:46 -0400

    Wrote some of the code to determine percent errors

--- M3_Benchmark_124_23.m
+++ M3_Benchmark_124_23.m
@@ -1,10 +1,10 @@
-function[] = M3_main_124_23_inagel()
+function[] = M3_Benchmark_124_23(TimeAcc, Tau, Vi, Vf, indx);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % ENGR 132 
 % Run the skeleton function that runs all the functions
 %
 % Function Call
-% [] = M3_main_124_23_inagel()
+% [] = M3_Benchmark_124_23()
 %
 % Input Arguments
 % None
@@ -14,110 +14,79 @@ function[] = M3_main_124_23_inagel()
 %
 % Assignment Information
 %   Assignment:     M3
-%   Team member:    Isaac Nagel, inagel@purdue.edu
 %   Team ID:        124-23
 %   Academic Integrity:
 %     [N/A] We worked with one or more peers but our collaboration
 %        maintained academic integrity.
-
-
-
 % 
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% ____________________
 %% INITIALIZATION
-% Pull all data from the spreadsheet
-testData = readmatrix('Sp25_cruiseAuto_M3benchmark_data.csv','NumHeaderLines',1);
 
-numCol = width(testData);
+% Initialize Benchmark values:
 
-% Pull in ALL the columns for graphical analysis
-time = testData(:,1);
+% Compact Car
+compTimeAccBench = 6.21;
+compTauBench = 1.51;
+compViBench = -0.09;
+compVfBench = 25.08;
 
-% Start index at 2 to avoid the first column of time values
-indx = 2;
+% Sedan Car
+sedanTimeAccBench = 9.39;
+sedanTauBench = 1.96;
+sedanViBench = -0.22;
+sedanVfBench = 24.72;
 
-% Create boundary variables for the left and right bound
-t = [0:30];
-t_s_left = 4.50;
-t_s_right = 6.00;
-tau_l = 1.26;
-tau_r = 3.89;
-y_L_left = 1.10;
-y_L_right = -0.90;
-y_h_left = 25.82;
-y_h_right = 23.36;
+% SUV Car
+SUVTimeAccBench = 6.85;
+SUVTauBench = 2.80;
+SUVViBench = 0.19;
+SUVVfBench = 25.18;
 
-% Create blank vectors for graphing
-y_left = zeros(1,30);
-y_right = zeros(1,30);
+% Create new index
+newIdx = indx-1;
 
 %% ____________________
 %% CALCULATIONS
 
+% Part 4a: Calculate SSE to evaluate data cleaning
 
-% Calculate the left bound graph line
-for i = t
-    if i >= 0 && i <= t_s_left
-        y_left(i+1) = y_L_left;
-    else 
-        y_left(i+1) = y_L_left + (1 - exp((-1).*((i-t_s_left)./(tau_l)))).*(y_h_left - y_L_left);
-    end 
+
+
+% Part 4b: Algo accuracy analysis 
+
+% Calculate percent error in TimeAcc:
+
+if newIdx == 1
+    % Calc here
+    % Find TimeAccc error
+    errorTimeAcc = (abs(compTimeAccBench - TimeAcc) ./ compTimeAccBench) .* 100;
+    % Find Tau error
+    errorTau = (abs(compTauBench - Tau) ./ compTauBench) .* 100;
+    % Find Vi error
+    errorVi = (abs(compViBench - Vi) ./ compViBench) .* 100;
+    % Fnd Vf
+    errorVf = (abs(compVfBench - Vf) ./ compVfBench) .* 100;
+    % Output all data
+    fprintf('The percent error in the acceleration start time is %0.2f %% \n',errorTimeAcc);
+    fprintf('The percent error in Tau is %0.2f %% \n', errorTau);
+    fprintf('The percent error in the initial velocity is %0.2f %% \n',errorVi);
+    fprintf('The percent error in the final velocity is %0.2f %% \n',errorVf);
+
+elseif newIdx == 2
+    % Calc here
+elseif newIdx == 3
+    % Calc here
+else
+    % Display error message?
 end
-
-% Calculate the right bound graph line
-
-for i = t
-    if i >= 0 && i <= t_s_right
-        y_right(i+1) = y_L_right;
-    else 
-        y_right(i+1) = y_L_right + (1 - exp((-1).*((i-t_s_right)./(tau_r)))).*(y_h_right - y_L_right);
-    end 
-end
-
-
-% Iterate through the data until we cover all of the columns
-while indx <= numCol
-    rawY = testData(:,indx);
-
-    [TimeClean, SpeedClean] = M3_sub1_124_23_sdimeola(time, rawY);
-
-    [TimeAcc, yL] = M3_sub2_124_23_thussp(TimeClean, SpeedClean);
-    fprintf('TimeAcc: %0.2f\n',TimeAcc);
-
-   
-  [Vi, Vf] = M3_sub3_124_23_muell147(TimeClean, SpeedClean, TimeAcc);
-
-  [Tau] = M3_sub4_124_23_thussp(TimeClean, SpeedClean, Vf, yL,TimeAcc);
-
-
-  calcLine = zeros(1,30);
-for idx = t
-    if idx >= 0 && idx <= TimeAcc
-        calcLine(idx+1) = Vi;
-    else 
-        calcLine(idx+1) = Vi + (1 - exp((-1).*((idx-TimeAcc)./(Tau)))).*(Vf - Vi);
-    end 
-end
-
-figure; 
-plot(t, calcLine);
-hold on
-plot(t,y_right);
-plot(t,y_left);
-
-    indx = indx +1;
-
-end
-
 
 %% ____________________
 %% FORMATTED TEXT/FIGURE DISPLAYS
 
-% Test numcol function
-%fprintf('The number of columns is %0.2f \n',numCol);
+
 
 
 
