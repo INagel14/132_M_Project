af671911dc5d5a1b44bd1ff2864bca54a56e672b
INagel14 <isaacnagel2024@gmail.com>
2025-04-23 10:00:35 -0400

    Moved code to subfunction performance

--- M3_main_124_23_inagel.m
+++ M3_main_124_23_inagel.m
@@ -40,43 +40,12 @@ time = testData(:,1);
 % Start index at 2 to avoid the first column of time values
 indx = 2;
 
-% Create boundary variables for the left and right bound
-t = [0:30];
-t_s_left = 4.50;
-t_s_right = 6.00;
-tau_l = 1.26;
-tau_r = 3.89;
-y_L_left = 1.10;
-y_L_right = -0.90;
-y_h_left = 25.82;
-y_h_right = 23.36;
-
-% Create blank vectors for graphing
-y_left = zeros(1,30);
-y_right = zeros(1,30);
 
 %% ____________________
 %% CALCULATIONS
 
 
-% Calculate the left bound graph line
-for i = t
-    if i >= 0 && i <= t_s_left
-        y_left(i+1) = y_L_left;
-    else 
-        y_left(i+1) = y_L_left + (1 - exp((-1).*((i-t_s_left)./(tau_l)))).*(y_h_left - y_L_left);
-    end 
-end
 
-% Calculate the right bound graph line
-
-for i = t
-    if i >= 0 && i <= t_s_right
-        y_right(i+1) = y_L_right;
-    else 
-        y_right(i+1) = y_L_right + (1 - exp((-1).*((i-t_s_right)./(tau_r)))).*(y_h_right - y_L_right);
-    end 
-end
 
 
 % Iterate through the data until we cover all of the columns
@@ -94,20 +63,6 @@ while indx <= numCol
 
   M3_Benchmark_124_23(TimeAcc, Tau, Vi, Vf, indx);
 
-%   calcLine = zeros(1,30);
-% for idx = t
-%     if idx >= 0 && idx <= TimeAcc
-%         calcLine(idx+1) = Vi;
-%     else 
-%         calcLine(idx+1) = Vi + (1 - exp((-1).*((idx-TimeAcc)./(Tau)))).*(Vf - Vi);
-%     end 
-% end
-% 
-% figure; 
-% plot(t, calcLine);
-% hold on
-% plot(t,y_right);
-% plot(t,y_left);
 
     indx = indx +1;
 
